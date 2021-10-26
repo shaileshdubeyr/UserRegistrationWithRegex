41a720532a843d8ac4931f2747f07af31e8009f3
shaileshdubeyr <54022188+shaileshdubeyr@users.noreply.github.com>
2021-10-27 03:00:24 +0530

    Delete Algorithm directory

--- Algorithm/src/main/java/PatternExample.java
+++
@@ -1,7 +0,0 @@
-import java.util.regex.*;
-public class PatternExample {
-    public static void main(String[] args) {
-        boolean result = Pattern.matches("[7-9]{1}\\d{9}","8723456789");
-        System.out.println(result);
-    }
-}
