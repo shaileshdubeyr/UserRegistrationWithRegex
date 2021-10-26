2367ef441c46a32216be151d206ff9c560da3e3e
shaileshdubeyr <54022188+shaileshdubeyr@users.noreply.github.com>
2021-10-27 00:24:34 +0530

    Update UserDetails.java
    Extra sout Deleted

--- gradleValidationProject/src/main/java/UserDetails.java
+++ gradleValidationProject/src/main/java/UserDetails.java
@@ -7,9 +7,7 @@ public class UserDetails {
 
     public boolean firstName() {
         System.out.println("Enter the name");
-        boolean matchesString = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,15}$", "Shailesh");
-        System.out.println("the result is " + matchesString);
-        System.out.println("the result is " + matchesString);
-        return matchesString;
+        boolean mathcPattern = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,15}$", "Shailesh");
+        return matchPattern;
     }
 }
