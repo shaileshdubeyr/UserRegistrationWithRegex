4621400417d12c0cbec2367e3cedc0180a8d3eb4
SHAILESH <shaileshdubeyr@gmail.com>
2021-10-26 08:03:19 +0530

    A

--- gradleValidationProject/src/main/java/UserDetails.java
+++ gradleValidationProject/src/main/java/UserDetails.java
@@ -7,7 +7,7 @@ public class UserDetails {
 
     public boolean password() {
         System.out.println("Password test Test");
-        boolean matchesString = Pattern.matches("^[A-Z{1,}a-z]{8}$", "kushdube");
+        boolean matchesString = Pattern.matches("^[A-Z{1,}0-9{1,}a-z]{8}$", "ku13Dube");
         return matchesString;
     }
 }
