793edcc2d3ca38b92cd570f831f3ece4c2500cfc
shaileshdubeyr <54022188+shaileshdubeyr@users.noreply.github.com>
2021-10-27 00:42:27 +0530

    Update UserDetails.java
    Refactor in Naming convention

--- gradleValidationProject/src/main/java/UserDetails.java
+++ gradleValidationProject/src/main/java/UserDetails.java
@@ -7,9 +7,7 @@ public class UserDetails {
 
     public boolean lastName() {
         System.out.println("Enter the name");
-        boolean matchesString = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,15}$", "Shailesh");
-        System.out.println("the result is " + matchesString);
-        System.out.println("the result is " + matchesString);
-        return matchesString;
+        boolean matchesPattern = Pattern.matches("^[A-Z]{1}[a-zA-Z]{3,15}$", "Shailesh");
+        return matchesPattern;
     }
 }
